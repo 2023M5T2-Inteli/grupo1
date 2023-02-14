674b786280d2aa71887a01ea54845d19c7c64585
Elisa Flemer <elisaflemer@gmail.com>
2023-02-14 18:53:52 -0300

    Correct folder structure with shared env for robot, backend and react frontend

--- src/embedded/robot.py
+++ src/embedded/robot.py
@@ -12,6 +12,12 @@ device = pydobot.Dobot(port=port, verbose=False)
 (x, y, z, r, j1, j2, j3, j4) = device.pose()
 print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
 
-device.move_to(x + 50, y + 50, z - 10, r+ 200, wait=True)
+device.move_to(10, -227, -23, -57, wait=True)
+device.move_to(145, -231, -41, -57, wait=True)
+device.move_to(156, -229, 110, -55, wait=True)
+device.move_to(251, -79, 114, -17, wait=True)
+device.move_to(281, -85, -32, -16, wait=True)
+device.move_to(285, 64, -29, 12, wait=True)
+
 
 device.close()
