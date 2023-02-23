f58c055329d710ae0efc63d315a1b0676935653e
LucaSarhan <99192966+LucaSarhan@users.noreply.github.com>
2023-02-23 16:23:03 -0300

    mudei os arquivos onde estava localizado minhas mudanças para os arquivos ja existentes

---
+++ src/frontend/src/Initial.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './Tela.css'
+
+function Initial() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <div class="button">
+      <button class="button-solid"> 
+      </button>
+    </div>
+    </div>
+  )
+}
+
+export default App
