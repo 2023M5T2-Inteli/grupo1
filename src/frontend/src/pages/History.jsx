1e0f562d5f3c02c3d7c462951d85be4648191c03
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-03-08 16:42:56 -0300

    Criação de rotas

---
+++ src/frontend/src/pages/History.jsx
@@ -0,0 +1,13 @@
+
+function History() {
+return (
+    <div>
+        <h1>Bem-vindo à minha página inicial!</h1>
+        <h1>Bem-vindo à minha página inicial!</h1>
+        <h1>Bem-vindo à minha página inicial!</h1>
+    </div>
+    
+);
+}
+
+export default History;
