1c2ff9a09d684ba28146dea7718d951e518e8e28
Elisa Flemer <elisaflemer@gmail.com>
2023-03-08 17:08:44 -0300

    four magnets in raspberry

--- src/embedded/motor.py
+++ src/embedded/motor.py
@@ -2,7 +2,7 @@
 # e então envia requisições continuamente ao servidor para descobrir o estado desejado para os atuadores. Segundo as informações recebidas, ele liga ou desliga os componentes externos (ímã e bomba)
 
 # Importa bibliotecas necessárias
-import machine  # Controla o Raspberry
+from machine import PWM, Pin # Controla o Raspberry
 import time  # Permite adicionar delays ao programa
 import network  # Permite conexão
 import urequests  # Permite envio de requisições
@@ -14,40 +14,51 @@ password = 'QazWsx@123'
 host = 'http://10.128.20.240:5000'
 
 # Definição dos pinos do ímã como PWM e output
-magnet_pin_1 = machine.PWM(machine.Pin(16, machine.Pin.OUT))
-magnet_pin_2 = machine.PWM(machine.Pin(15, machine.Pin.OUT))
-# Define frequências dos pinos dos ímãs
-magnet_pin_1.freq(1000)
-magnet_pin_2.freq(1000)
+magnets_a = [PWM(machine.Pin(16, machine.Pin.OUT)),
+           PWM(machine.Pin(16, machine.Pin.OUT))]
+
+magnets_b = [PWM(machine.Pin(16, machine.Pin.OUT)),
+           PWM(machine.Pin(16, machine.Pin.OUT))]
+
+for magnet in magnets_a:
+    magnet.freq(1000)
+    
+for magnet in magnets_b:
+    magnet.freq(1000)
 
 # Definição dos pinos da bomba como pinos digitais de output. Como a intensidade da bomba não precisa ser dinâmica, não precisamos utilizar PWM
-pump_pin_1 = machine.Pin(12, machine.Pin.OUT)
-pump_pin_2 = machine.Pin(13, machine.Pin.OUT)
+shaker_pump_pin_1 = Pin(12, Pin.OUT)
+shaker_pump_pin_2 = Pin(13, Pin.OUT)
+
+cleaner_pump_pin_1 = machine.Pin(14, machine.Pin.OUT)
+cleaner_pump_pin_2 = machine.Pin(15, machine.Pin.OUT)
 
 # Esta função liga o ímã na voltagem desejada. A integração com o front passando essa voltagem
 # ainda não foi implementada, então, por enquanto, essa função é sempre executada com argumento
 # 'hardcoded' 12.
-def enable_magnet(voltage):
-    # Seguindo a lógica do ponte H, desligamos um dos pinos e ligamos a proporção desejada no outro
-    magnet_pin_2.duty_u16(0)
-    # O argumento dessa função encontra o valor proporcional de duty cycle a ser enviado para
-    # o ímã, transformando um valor na escala 0-12 para um valor na escala 0-65535
-    magnet_pin_1.duty_u16(int(voltage / 12.0 * 65535))
+def enable_magnets(voltage):
+    max_voltage = 12.0
+    max_duty_u16 = 65535
+    magnets_a[0].duty_u16(0)
+    magnets_b[0].duty_u16(0)
+
+    magnets_a[1].duty_u16(int(voltage / max_voltage * max_duty_u16))
+    magnets_b[1].duty_u16(int(voltage / max_voltage * max_duty_u16))
 
 # Desliga o ímã, enviando a ambos os pinos 0
 def disable_magnet():
-    magnet_pin_1.duty_u16(0)
-    magnet_pin_2.duty_u16(0)
+    magnets_a[1].duty_u16(0)
+    magnets_b[1].duty_u16(0)
 
 # Liga a bomba, enviando a apenas um pino 1, seguindo a lógica da ponte H
 def enable_pump():
-    pump_pin_1.value(1)
-    pump_pin_2.value(0)
+    shaker_pump_pin_1.value(1)
+    shaker_pump_pin_2.value(0)
 
 # Desliga a bomba, enviando a ambos os pinos 0
 def disable_pump():
-    pump_pin_1.value(0)
-    pump_pin_2.value(0)
+    shaker_pump_pin_1.value(0)
+    shaker_pump_pin_2.value(0)
 
 # Conecta à rede local
 def connect():
@@ -82,7 +93,7 @@ try:
         
         # Liga ímã com voltagem 12 se o valor lido no servidor for maior que zero
         if (int(magnet_state.text)):
-            enable_magnet(12)
+            enable_magnets(12)
         else: # Desliga se for 0
             disable_magnet()
 
