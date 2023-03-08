3d4deeef556406ba7d91b8f1181702df76e8e04f
Elisa Flemer <elisaflemer@gmail.com>
2023-03-08 17:14:40 -0300

    added cleaner_pump to raspberry

--- src/embedded/motor.py
+++ src/embedded/motor.py
@@ -2,7 +2,7 @@
 # e então envia requisições continuamente ao servidor para descobrir o estado desejado para os atuadores. Segundo as informações recebidas, ele liga ou desliga os componentes externos (ímã e bomba)
 
 # Importa bibliotecas necessárias
-from machine import PWM, Pin # Controla o Raspberry
+from machine import PWM, Pin, reset # Controla o Raspberry
 import time  # Permite adicionar delays ao programa
 import network  # Permite conexão
 import urequests  # Permite envio de requisições
@@ -14,11 +14,11 @@ password = 'QazWsx@123'
 host = 'http://10.128.20.240:5000'
 
 # Definição dos pinos do ímã como PWM e output
-magnets_a = [PWM(machine.Pin(16, machine.Pin.OUT)),
-           PWM(machine.Pin(16, machine.Pin.OUT))]
+magnets_a = [PWM(Pin(16, Pin.OUT)),
+           PWM(Pin(16, Pin.OUT))]
 
-magnets_b = [PWM(machine.Pin(16, machine.Pin.OUT)),
-           PWM(machine.Pin(16, machine.Pin.OUT))]
+magnets_b = [PWM(Pin(16, Pin.OUT)),
+           PWM(Pin(16, Pin.OUT))]
 
 for magnet in magnets_a:
     magnet.freq(1000)
@@ -27,11 +27,13 @@ for magnet in magnets_b:
     magnet.freq(1000)
 
 # Definição dos pinos da bomba como pinos digitais de output. Como a intensidade da bomba não precisa ser dinâmica, não precisamos utilizar PWM
-shaker_pump_pin_1 = Pin(12, Pin.OUT)
-shaker_pump_pin_2 = Pin(13, Pin.OUT)
+shaker_pump = [Pin(12, Pin.OUT), Pin(13, Pin.OUT)]
 
-cleaner_pump_pin_1 = machine.Pin(14, machine.Pin.OUT)
-cleaner_pump_pin_2 = machine.Pin(15, machine.Pin.OUT)
+cleaner_pump = [Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
+
+def init():
+    disable_magnets()
+    disable_pumps()
 
 # Esta função liga o ímã na voltagem desejada. A integração com o front passando essa voltagem
 # ainda não foi implementada, então, por enquanto, essa função é sempre executada com argumento
@@ -46,19 +48,21 @@ def enable_magnets(voltage):
     magnets_b[1].duty_u16(int(voltage / max_voltage * max_duty_u16))
 
 # Desliga o ímã, enviando a ambos os pinos 0
-def disable_magnet():
+def disable_magnets():
     magnets_a[1].duty_u16(0)
     magnets_b[1].duty_u16(0)
 
 # Liga a bomba, enviando a apenas um pino 1, seguindo a lógica da ponte H
-def enable_pump():
-    shaker_pump_pin_1.value(1)
-    shaker_pump_pin_2.value(0)
+def enable_pumps():
+    shaker_pump[0].value(0)
+    cleaner_pump[0].value(0)
+    shaker_pump[1].value(1)
+    cleaner_pump[1].value(1)
 
 # Desliga a bomba, enviando a ambos os pinos 0
-def disable_pump():
-    shaker_pump_pin_1.value(0)
-    shaker_pump_pin_2.value(0)
+def disable_pumps():
+    shaker_pump[1].value(0)
+    cleaner_pump[1].value(0)
 
 # Conecta à rede local
 def connect():
@@ -80,6 +84,7 @@ def connect():
 
 # Código a ser executado quando o Raspberry é ligado
 try:
+    init()
     connect()  # Conecta à rede local
     while True:  # Loop para ler estado desejado dos atuadores
         # Ainda não descobrimos como processar um objeto json em micropython. Por isso,
@@ -95,17 +100,17 @@ try:
         if (int(magnet_state.text)):
             enable_magnets(12)
         else: # Desliga se for 0
-            disable_magnet()
+            disable_magnets()
 
         # Liga bomba se o valor lido no servidor for maior que zero
         if (int(pump_state.text)):
-            enable_pump()
+            enable_pumps()
         else: # Desliga se for 0
             print('disable pump')
-            disable_pump() 
+            disable_pumps() 
 
         # Espera 0.1s antes de reiniciar o loop
         time.sleep(0.1)
         
 except KeyboardInterrupt:
-    machine.reset()
+    reset()
