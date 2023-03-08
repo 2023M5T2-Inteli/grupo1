16c36494dd9f057d6c3b4e3731f4a79ad93e0e7b
Emanuele Morais <emanuele.lmorais@gmail.com>
2023-03-08 17:13:59 -0300

    Inicio pagina historico

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -3,10 +3,6 @@ outros scrips da pasta "components".
 */
 
 // Importa recursos necessários
-import { useState, useEffect } from "react"; // Hook para controlar estado dos componentes
-import Axios from "axios"; // Biblioteca para fazer requisições HTTP
-import bgWaves from "./assets/bg-waves.png"; // Vetor de decoração na base da página
-import Button from "./components/Button";
 import { Routes, Route, BrowserRouter as Router, BrowserRouter } from 'react-router-dom';
 import History from './pages/History';
 import Begin from './pages/Begin';
@@ -18,7 +14,7 @@ function App() {
     <div>
       <BrowserRouter>
       <Routes>
-            <Route path='/teste' element={<Begin />} />
+            <Route path='/' element={<Begin />} />
             <Route path='/history' element={<History />}/>
       </Routes>
       </BrowserRouter>
--- src/frontend/src/pages/History.jsx
+++ src/frontend/src/pages/History.jsx
@@ -2,9 +2,14 @@
 function History() {
 return (
     <div>
-        <h1>Bem-vindo à minha página inicial!</h1>
-        <h1>Bem-vindo à minha página inicial!</h1>
-        <h1>Bem-vindo à minha página inicial!</h1>
+        <h1 className="font-montserrat font-bold text-4xl box-border p-16">Histórico de ensaios</h1>
+        <select>
+        <option value="">Selecione uma opção</option>
+        <option value="opcao1">Opção 1</option>
+        <option value="opcao2">Opção 2</option>
+        <option value="opcao3">Opção 3</option>
+</select>
+        
     </div>
     
 );
