97f4a48843996f4b2878fd641634f5b83541de16
Elisa Flemer <elisaflemer@gmail.com>
2023-03-27 16:39:29 -0300

    correct frontend folders

--- src/frontend/src/utils/strings.js
+++
@@ -1,7 +0,0 @@
-// Script de funções para manipular strings
-
-function capitalizeFirstLetter(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
-
-export default capitalizeFirstLetter
