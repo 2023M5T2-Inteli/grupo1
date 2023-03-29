c0114acc39a6aa703133de6d6b13c7c6ab8b0b1a
Elisa Flemer <elisaflemer@gmail.com>
2023-03-29 17:20:17 -0300

    rotas funcionando exceto cycles

--- src/backend/magnetum/controllers/cycle.py
+++ src/backend/magnetum/controllers/cycle.py
@@ -3,44 +3,26 @@
 from magnetum.models.tables.cycle import Cycle
 from datetime import datetime
 from magnetum.controllers.magnet import magnets
+from magnetum.config.db import session
 
 from flask import request
 
 # Pegar todos os ciclos
-def get_all():
-    try:
-        cycles = Cycle.query.all()
-        response = [cycle.return_json() for cycle in cycles]
-        return response, 200
-
-    except Exception as e:
-        response = {'status': 'error', 'message': str(e)}
-        return response, 500
+def get_all(
 
 # Pegar ciclo por id
 def get_by_id(id):
     try:
-        cycle = Cycle.query.filter(Cycle.id == id).first()
+        cycle = session.query(Cycle).filter(Cycle.id == id).first()
         return cycle.return_json(), 200
     except Exception as e:
         response = {'status': 'error', 'message': str(e)}
         return response, 500
 
 # Criar ciclo com dados de request, em JSON
-def create(request):
+def create(routine_id):
     try:
-        cycle = Cycle(routine_id=request.json['routine_id'], initiated_at=datetime.now().isoformat(), magnet_intensity=magnets.get_intensity()) # Intensidade é adicionada automaticamente
-        cycle.save()
-        return cycle.return_json(), 201
-    except Exception as e:
-        response = {'status': 'error', 'message': str(e)}
-        return response, 500
-
-# Atualizar ciclo com dados de request, em JSON
-def update(request, id):
-    try:
-        cycle = Cycle.query.filter(Cycle.id == id).first()
-        cycle.finished_at = datetime.now().isoformat()
+        cycle = Cycle(routine_id=routine_id, magnet_intensity=magnets.get_intensity()) # Intensidade é adicionada automaticamente
         cycle.save()
         return cycle.return_json(), 201
     except Exception as e:
